45858abe400b3dfae291b4ba7154c088a3d27bd2
alegroni <ajmarzano@gmail.com>
2019-05-06 00:17:59 -0300

    draggable 50/50

--- js/events.js
+++ js/events.js
@@ -22,6 +22,7 @@ var $comparisonSlider = $('.comparison-slider-container'),
 // tween 
 tl.set($caption, {autoAlpha:0, yPercent:-100}, 0);
 tl.to($sliderLeft, 0.7, {width:$startPosition, ease:Back.easeOut.config(1.7)}, 0);
+// tl.to($sliderRight, 0.7, {width:$startPosition, ease:Back.easeOut.config(1.7)}, 0);
 tl.to($handle, 0.7, {x:$startPosition, ease:Back.easeOut.config(1.7)}, 0);
 tl.staggerTo($caption, 0.7, {autoAlpha:0, yPercent:0, ease:Back.easeInOut.config(3)}, -0.3, 0);
 
@@ -43,9 +44,20 @@ Draggable.create($handle, {
   }
 });
 
+function changeRight() {
+  var leftWidth = parseInt($('.slider-left').width());
+
+  ($sliderRight).css({
+    width: $comparisonSliderWidth - leftWidth
+  });
+}
 function onHandleDrag() {
-  console.log('drag', this)
-  TweenMax.set($sliderLeft, {width:this.endX}, 0);
+  // console.log('drag', this)
+  tSet = TweenMax.set($sliderLeft, {width:this.endX}, 0);
+  console.log(tSet.vars["css"].width);
+  changeRight();
+
+ 
 
   // show/hide caption if you drag past the center of the container, towards left/right direction.
   if (this.endX >= ((this.maxX/2)+10) && this.getDirection() === 'right') {
@@ -76,15 +88,22 @@ function hideBothCaptions() {
 }
 
 function slideHandleTo(_pos){
-  TweenMax.to($sliderLeft, 0.7, {width:($comparisonSliderWidth/100)*_pos, ease:Power2.easeOut}, 0);
+TweenMax.to($sliderLeft, 0.7, {width:($comparisonSliderWidth/100)*_pos, ease:Power2.easeOut}, 0);
+TweenMax.to($sliderRight, 0.7, {
+   width: ($comparisonSliderWidth / 100) * _pos,
+   ease: Power2.easeOut
+ }, 0);
   TweenMax.to($handle, 0.7, {x:($comparisonSliderWidth/100)*_pos, ease:Power2.easeOut}, 0);
+
 }
 
 // CLICK HANDLERS
 
+
 $btnExpandLeft.on('click', function(){
   slideHandleTo(100);
   showLeftCaption();
+  
 })
 
 $btnExpandCenter.on('click', function(){
